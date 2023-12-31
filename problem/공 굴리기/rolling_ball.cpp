7a5573acca19d120fb5024ef53d0fb8de0bf1004
pnuth <85662273+radicalparty@users.noreply.github.com>
2023-12-31 22:31:08 +0900

    Create rolling_ball.cpp

---
+++ problem/공 굴리기/rolling_ball.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ll n, k;
+ll weight(ll l, ll r){
+    return l > r;
+}
+ll f(ll l, ll r, ll k){
+    if (l == r)  return l;
+    ll m = (l + r) >> 1, nk = (k + 1) >> 1;
+    if (k % 2)  return f(l, m + weight(l, r), nk);
+    return f(r, m + weight(r, l), nk);
+}
+ll solve(){
+    cin >> n >> k;
+    for (ll i = n - 1; i >= 0; i--){
+        if ((1 << i) >= k){
+            return f((1 << (i + 1)) - 1, 1 << i, k);
+        }
+        k -= 1 << i;
+    }
+}
+int main(){
+    cin.tie(0); ios_base::sync_with_stdio(false);
+    cout << solve();
+    return 0;
+}
